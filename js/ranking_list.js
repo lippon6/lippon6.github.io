177e3bf79befaa2260a2d318b4222136b812d595
libang <1004293638@qq.com>
2019-11-30 17:14:26 +0800

    fixed

---
+++ js/ranking_list.js
@@ -0,0 +1,18 @@
+//排行榜
+var rankingList = {
+
+	rowSpace:20,
+	colSpace:100,
+	nameSpace:70,
+	scoreSpace:40,
+	quantity:5,
+
+	head:["1st:", "2nd:" , "3rd:", "4th:", "5th:"],
+	name:["***", "***", "***", "***", "***"],
+	score:["***", "***", "***", "***", "***"],
+
+}
+
+
+
+
--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+<<<<<<< HEAD
 window.onload = function(){
+=======
+window.onload = function () {
+>>>>>>> 449c68dc4f3167821f4f53bedf22106464b98d42
     var keyCode = {"left": "37", "right": "39", "up": "38", "down": "40"};
     var keyBuf = {};
     var myHeight = 600, myWidth = 400;
     var globalInf = {
         times: 0,
+<<<<<<< HEAD
         timeAdd: Math.PI/60,
         score: 0,
         tip: true,
@@ -29,6 +34,18 @@ window.onload = function(){
         y: myHeight * 3/4,
         width:50,
         height:50,
+=======
+        timeAdd: 0.05,
+        score: 0,
+        tip: true,
+    };
+
+    var plane = {
+        x: myWidth / 2,
+        y: myHeight * 3 / 4,
+        width: 50,
+        height: 50,
+>>>>>>> 449c68dc4f3167821f4f53bedf22106464b98d42
         speedX: 0,
         speedY: 0,
         speedMax: 150,
@@ -37,6 +54,7 @@ window.onload = function(){
         accControl: 0.5,
         friction: 0.05,
         direction: false,
+<<<<<<< HEAD
 
         draw: function(){
 	        envir.translate(plane.x, plane.y);
@@ -88,6 +106,12 @@ window.onload = function(){
 
 	//障碍物
     function obstacle(x, y, speedX, speedY, width, height, beginPos){
+=======
+    };
+
+
+    function obstacle(x, y, speedX, speedY, width, height, beginPos) {
+>>>>>>> 449c68dc4f3167821f4f53bedf22106464b98d42
         this.x = x;
         this.y = y;
         this.speedX = speedX;
@@ -95,7 +119,10 @@ window.onload = function(){
         this.width = width;
         this.height = height;
         this.beginPos = beginPos;
+<<<<<<< HEAD
 
+=======
+>>>>>>> 449c68dc4f3167821f4f53bedf22106464b98d42
     }
 
 
@@ -106,7 +133,11 @@ window.onload = function(){
         1,
         150,
         30,
+<<<<<<< HEAD
         -800
+=======
+        -500
+>>>>>>> 449c68dc4f3167821f4f53bedf22106464b98d42
     );
 
     var obstacle2 = new obstacle(
@@ -116,6 +147,7 @@ window.onload = function(){
         1,
         150,
         30,
+<<<<<<< HEAD
         -800
     );
 
@@ -214,10 +246,49 @@ window.onload = function(){
 
     function update(){
         (function sys_init(){
+=======
+        -300
+    );
+
+    var c = document.getElementById("myCanvas");
+
+    addEventListener("keydown", function (e) {
+        keyBuf[e.keyCode] = true;
+        globalInf.tip = false;
+    }, false);
+    addEventListener("keyup", function (e) {
+        delete keyBuf[e.keyCode];
+    }, false);
+    var myCanvas = window.document.getElementById("myCanvas");
+    myCanvas.setAttribute("width", myWidth);
+    myCanvas.setAttribute("height", myHeight);
+
+    function game_init() {
+        delete keyBuf[keyCode.up];
+
+        plane.accelerationX = 0;
+        plane.accelerationY = 0;
+        plane.speedY = 0;
+        plane.x = myWidth / 2;
+        plane.y = myHeight * 3 / 4;
+        obstacle1.y = Math.random() * obstacle1.beginPos;
+        obstacle2.y = Math.random() * obstacle2.beginPos;
+
+        globalInf.times = 0;
+        globalInf.score = 0;
+        globalInf.tip = true;
+
+    }
+
+
+    function update() {
+        (function sys_init() {
+>>>>>>> 449c68dc4f3167821f4f53bedf22106464b98d42
             plane.accelerationX = 0;
             plane.accelerationY = 0;
         }());
 
+<<<<<<< HEAD
         (function event_trigger(){
             if(keyBuf[keyCode.left]){
                 plane.accelerationX = plane.accControl;
@@ -230,10 +301,25 @@ window.onload = function(){
                 plane.accelerationY = plane.accControl*(1 + 0.1*globalInf.score);
             }
             if(keyBuf[keyCode.down]){
+=======
+        (function event_trigger() {
+            if (keyBuf[keyCode.left]) {
+                plane.accelerationX = plane.accControl;
+            }
+            if (keyBuf[keyCode.right]) {
+                plane.accelerationX = -plane.accControl;
+            }
+            if (keyBuf[keyCode.up]) {
+
+                plane.accelerationY = plane.accControl * (1 + 0.1 * globalInf.score);
+            }
+            if (keyBuf[keyCode.down]) {
+>>>>>>> 449c68dc4f3167821f4f53bedf22106464b98d42
                 plane.accelerationY = -plane.accControl;
             }
         }());
 
+<<<<<<< HEAD
         (function step(){
 
             plane.move();
@@ -248,10 +334,59 @@ window.onload = function(){
                     game_over();
                 }else if((obstacle1.y-plane.y) > 0 && (obstacle1.y-plane.y) < (plane.height/2)){
                     game_over();
+=======
+        (function step() {
+            if (plane.direction) {
+                globalInf.times += globalInf.timeAdd;
+            } else {
+                globalInf.times -= globalInf.timeAdd;
+            }
+
+            if (globalInf.times > Math.PI * 2) {
+                plane.direction = false;
+                globalInf.times = Math.PI * 2;
+            } else if (globalInf.times < 0) {
+                plane.direction = true;
+                globalInf.times = 0;
+            }
+
+            plane.speedX = plane.speedMax * Math.cos(globalInf.times);
+            plane.x = myWidth / 2 - plane.speedX;
+
+            plane.speedY += plane.accelerationY;
+            plane.speedY *= (1 - plane.friction);
+
+            obstacle1.speedY = plane.speedY;
+            obstacle1.y += obstacle1.speedY;
+
+            obstacle2.speedY = plane.speedY;
+            obstacle2.y += obstacle1.speedY;
+
+            if (obstacle1.y > myHeight) {
+                obstacle1.y = Math.random() * Math.random() * obstacle1.beginPos;
+                globalInf.score += 1;
+            }
+            if (obstacle2.y > myHeight) {
+                obstacle2.y = Math.random() * Math.random() * obstacle2.beginPos;
+                globalInf.score += 1;
+            }
+
+        }());
+
+        (function hit_check() {
+            if ((plane.x - obstacle1.x) < (obstacle1.width + plane.width / 2)) {
+                if ((obstacle1.y - plane.y) < 0 && (plane.y - obstacle1.y) < (obstacle1.height + plane.height / 2)) {
+                    window.alert("game over");
+                    game_init();
+                } else if ((obstacle1.y - plane.y) > 0 && (obstacle1.y - plane.y) < (plane.height / 2)) {
+                    window.alert("game over");
+                    game_init();
+>>>>>>> 449c68dc4f3167821f4f53bedf22106464b98d42
                 }
             }
 
 
+<<<<<<< HEAD
             if((obstacle2.x-plane.x) < 0 && (plane.x-obstacle2.x) < (obstacle2.width +　plane.width/2)){
                 if((obstacle2.y-plane.y) < 0 && (plane.y-obstacle2.y) < (obstacle2.height + plane.height/2)){
                     game_over();
@@ -287,6 +422,51 @@ window.onload = function(){
         	plane.draw();
             rankingList.draw();
             globalInf.fill_tip();
+=======
+            if ((obstacle2.x - plane.x) < 0 && (plane.x - obstacle2.x) < (obstacle2.width + plane.width / 2)) {
+                if ((obstacle2.y - plane.y) < 0 && (plane.y - obstacle2.y) < (obstacle2.height + plane.height / 2)) {
+                    window.alert("game over");
+                    game_init();
+                } else if ((obstacle2.y - plane.y) > 0 && (obstacle2.y - plane.y) < (plane.height / 2)) {
+                    window.alert("game over");
+                    game_init();
+                }
+            }
+        }());
+
+    };
+    var envir = myCanvas.getContext("2d");
+
+    (function draw() {
+        update();
+        (function draw_init() {
+            envir.clearRect(0, 0, myWidth, myHeight);
+            envir.lineWidth = 5;
+            envir.strokeRect(0, 0, myWidth, myHeight);
+            envir.fillStyle = "green";
+
+            envir.translate(plane.x, plane.y);
+            if (plane.speedX != 0) {
+                envir.rotate(2 * Math.atan(plane.speedY / plane.speedX));
+                envir.fillRect(-plane.width / 2, -plane.height / 2, plane.width, plane.height);
+                envir.rotate(-2 * Math.atan(plane.speedY / plane.speedX));
+            } else {
+                envir.fillRect(-plane.width / 2, -plane.height / 2, plane.width, plane.height);
+            }
+
+
+            envir.translate(-plane.x, -plane.y);
+            envir.fillRect(obstacle1.x, obstacle1.y, obstacle1.width, obstacle1.height);
+            envir.fillRect(obstacle2.x, obstacle2.y, obstacle2.width, obstacle2.height);
+            envir.fillText("score:", 10, 20);
+            envir.fillText(globalInf.score, 50, 20)
+
+
+            if (globalInf.tip == true) {
+                envir.fillText("press ↑", myWidth / 2, myHeight / 2);
+            }
+            requestAnimationFrame(draw);
+>>>>>>> 449c68dc4f3167821f4f53bedf22106464b98d42
         }());
     }());
 }
